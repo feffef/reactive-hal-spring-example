777b97efcf596a62ff399c242b576af161fcb534
Steffen Sauder <ssauder@pro-vision.de>
2020-05-20 16:40:01 +0200

    removed CacheCustomizer component

--- src/main/java/com/github/feffef/reactivehalspringexample/common/CachingJsonResourceLoader.java
+++ src/main/java/com/github/feffef/reactivehalspringexample/common/CachingJsonResourceLoader.java
@@ -4,8 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Component;
 
-import com.github.feffef.reactivehalspringexample.CacheCustomizer;
-
 import io.reactivex.rxjava3.core.Single;
 import io.wcm.caravan.hal.microservices.api.client.JsonResourceLoader;
 import io.wcm.caravan.hal.microservices.api.common.HalResponse;
@@ -17,7 +15,7 @@ public class CachingJsonResourceLoader implements JsonResourceLoader {
 	private WebClientJsonResourceLoader webClientLoader;
 
 	@Override
-	@Cacheable(CacheCustomizer.JSON_RESOURCES_CACHE)
+	@Cacheable("CachingJsonResourceLoader")
 	public Single<HalResponse> loadJsonResource(String uri) {
 
 		return webClientLoader.loadJsonResource(uri).cache();
