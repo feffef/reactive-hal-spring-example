58a0969eb34779f2897085ab4a4ef3c094bdafa4
Steffen Sauder <ssauder@pro-vision.de>
2020-06-17 18:46:52 +0200

    code clean-up in MovkMvcJsonResourceLoader

--- src/test/java/com/github/feffef/reactivehalspringexample/common/MockMvcJsonResourceLoader.java
+++ src/test/java/com/github/feffef/reactivehalspringexample/common/MockMvcJsonResourceLoader.java
@@ -1,8 +1,14 @@
 package com.github.feffef.reactivehalspringexample.common;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.asyncDispatch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.request;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mock.web.MockHttpServletResponse;
@@ -15,6 +21,7 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -38,37 +45,56 @@ public class MockMvcJsonResourceLoader implements JsonResourceLoader {
 	@Override
 	public Single<HalResponse> loadJsonResource(String uri) {
 		try {
+			MockHttpServletResponse mvcResponse = getServletResponse(uri);
 
-			MvcResult asyncResult = mockMvc.perform(get(uri)).andExpect(MockMvcResultMatchers.request().asyncStarted())
-					.andReturn();
+			HalResponse halResponse = convertToHalResponse(mvcResponse);
 
-			MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.asyncDispatch(asyncResult))
-					.andExpect(status().isOk())
-					.andExpect(MockMvcResultMatchers.content().contentType(HalResource.CONTENT_TYPE)).andReturn();
-
-			MockHttpServletResponse mvcResponse = mvcResult.getResponse();
-
-			String jsonString = mvcResponse.getContentAsString();
-
-			assertThat(jsonString).isNotNull();
-
-			JsonNode jsonNode = JSON_FACTORY.createParser(jsonString).readValueAsTree();
-
-			HalResource hal = new HalResource(jsonNode);
-
-			HalResponse response = new HalResponse().withBody(hal).withStatus(mvcResponse.getStatus())
-					.withContentType(mvcResponse.getContentType());
-
-			String cacheControl = mvcResponse.getHeader("Cache-Control");
-			if (cacheControl != null) {
-				response = response.withMaxAge(CacheControlUtil.parseMaxAge(cacheControl));
-			}
-
-			return Single.just(response);
+			return Single.just(halResponse);
 
 		} catch (Exception e) {
-
 			return Single.error(new HalApiClientException("An exception occured", 500, uri, e));
 		}
 	}
+
+	private MockHttpServletResponse getServletResponse(String uri) throws Exception {
+
+		MvcResult asyncResult = mockMvc.perform(get(uri)).andExpect(request().asyncStarted())
+				.andReturn();
+
+		MvcResult mvcResult = mockMvc.perform(asyncDispatch(asyncResult))
+				.andExpect(status().isOk())
+				.andExpect(content().contentType(HalResource.CONTENT_TYPE))
+				.andReturn(); 
+
+		return mvcResult.getResponse();
+	}
+
+	private HalResource parseHalResource(MockHttpServletResponse mvcResponse)
+			throws UnsupportedEncodingException, IOException, JsonParseException {
+
+		String jsonString = mvcResponse.getContentAsString();
+		assertThat(jsonString).isNotNull();
+
+		JsonNode jsonNode = JSON_FACTORY.createParser(jsonString).readValueAsTree();
+
+		return new HalResource(jsonNode);
+	}
+
+	private HalResponse convertToHalResponse(MockHttpServletResponse mvcResponse)
+			throws UnsupportedEncodingException, IOException, JsonParseException {
+
+		HalResource hal = parseHalResource(mvcResponse);
+
+		HalResponse halResponse = new HalResponse()
+				.withBody(hal)
+				.withStatus(mvcResponse.getStatus())
+				.withContentType(mvcResponse.getContentType());
+
+		String cacheControl = mvcResponse.getHeader("Cache-Control");
+		if (cacheControl != null) {
+			halResponse = halResponse.withMaxAge(CacheControlUtil.parseMaxAge(cacheControl));
+		}
+		return halResponse;
+	}
+
 }
