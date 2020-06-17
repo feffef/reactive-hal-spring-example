7eceefb49c5f70630388984e918edbaf7014af2f
Steffen Sauder <ssauder@pro-vision.de>
2020-06-17 18:48:30 +0200

    organized imports

--- src/test/java/com/github/feffef/reactivehalspringexample/common/MockMvcJsonResourceLoader.java
+++ src/test/java/com/github/feffef/reactivehalspringexample/common/MockMvcJsonResourceLoader.java
@@ -15,8 +15,6 @@ import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
